3c98ec7e9180057d9973a033c0c7197ec3cc85e6
thryec <thryec@gmail.com>
2026-05-06 13:33:32 -0700

    perf(scan): tune SCAN_CHUNKS to 5000 and timeout to 10s

--- src/contracts/railgun-smart-wallet/V3/poseidon-merkle-accumulator.ts
+++ src/contracts/railgun-smart-wallet/V3/poseidon-merkle-accumulator.ts
@@ -21,9 +21,9 @@ import { recursivelyDecodeResult } from '../../../utils/ethers';
 import { TypedContractEvent, TypedEventLog } from '../../../abi/typechain/common';
 import { Nullifier } from '../../../models/formatted-types';
 
-const SCAN_CHUNKS = 10000;
+const SCAN_CHUNKS = 5000;
 const MAX_SCAN_RETRIES = 30;
-const EVENTS_SCAN_TIMEOUT = 5000;
+const EVENTS_SCAN_TIMEOUT = 10000;
 const SCAN_TIMEOUT_ERROR_MESSAGE = 'getLogs request timed out after 5 seconds.';
 
 export class PoseidonMerkleAccumulatorContract extends EventEmitter {
